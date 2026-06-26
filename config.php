b2db71174efd2a507606475595fc9b7254f03863
Kaushal Yadav <kaushalyadav0845@gmail.com>
2026-06-26 10:34:52 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php 
+
+//host
+$host ="localhost";
+
+//dbname
+$dbname = "auth-sys";
+
+
+//user
+$user ="root";
+
+
+//pass
+$pass ="";
+
+$conn = new PDO("mysql:host=$host; dbname=$dbname;", $user, $pass);
+
+// if($conn == true){
+//   echo "it's working fine";
+
+// } else{
+//   echo "Connection is worng: error";
+// }
+
+
+
+?>
